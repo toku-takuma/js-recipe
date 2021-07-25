d18d2359a897eb97b0d0d082b815868f84558ac0
toku-takuma <tokutaku1412@gmail.com>
2021-07-25 20:21:27 +0900

    4-1のワークを学習しました

--- 04ゼミ/week3/script.js
+++ 04ゼミ/week3/script.js
@@ -18,3 +18,24 @@ $(function () {
   });
 
 });
+
+function showElementAnimation() {
+  let element = document.getElementsByClassName("js-animation");
+  if (!element) return; // 要素がなかったら処理をキャンセル
+  let showTiming = window.innerHeight > 768 ? 200 : 40; // 要素が出てくるタイミングはここで調整
+  let scrollY = window.pageYOffset;
+  let windowH = window.innerHeight;
+
+  for (let i = 0; i < element.length; i++) {
+    let elemClientRect = element[i].getBoundingClientRect();
+    let elemY = scrollY + elemClientRect.top;
+    if (scrollY + windowH - showTiming > elemY) {
+      element[i].classList.add("is-show");
+    } else if (scrollY + windowH < elemY) {
+      // 上にスクロールして再度非表示にする場合はこちらを記述
+      element[i].classList.remove("is-show");
+    }
+  }
+}
+showElementAnimation();
+window.addEventListener("scroll", showElementAnimation);
